149dcafb235b8784f3a233044e671da05afb4614
Oleg Samoylov <quillaur@outlook.com>
2016-06-09 12:59:21 +1000

    WinJS ListView added.

---
+++ CC.Website/app/web/js/actions.js
@@ -0,0 +1,16 @@
+﻿var itemArray = [
+    { title: "Самое глубокое озеро в мире", text: "байкал" },
+    { title: "Самая большая пустыня в мире", text: "сахара" },
+    { title: "Вторая от Солнца планета", text: "венера" }
+];
+
+var items = [];
+
+itemArray.forEach(function (item) {
+    items.push(item);
+});
+
+WinJS.Namespace.define("Sample.ListView", {
+    data: new WinJS.Binding.List(items)
+});
+WinJS.UI.processAll();
