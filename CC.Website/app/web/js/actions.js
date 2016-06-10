b5413a94ba5a694abfc8cb5fe2b3b557116e5b0a
Oleg Samoylov <quillaur@outlook.com>
2016-06-11 01:40:21 +1000

    Улучшена обработка ввода данных.

--- CC.Website/app/web/js/actions.js
+++ CC.Website/app/web/js/actions.js
@@ -19,11 +19,21 @@
             text: document.getElementById("answer").value
         };
         itemsList.push(term);
+        document.getElementById("question").value = "";
+        document.getElementById("answer").value = "";
+    }
+
+    function check() {
+        var q = document.getElementById("question");
+        var a = document.getElementById("answer");
+        document.getElementById("addTerm").disabled = (q.value === "" || a.value === "") ? true : false;
     }
 
     WinJS.UI.processAll().then(function () {
         var element = document.body;
         element.querySelector("#addTerm").addEventListener("click", addTerm, false);
+        element.querySelector("#question").addEventListener("input", check, false);
+        element.querySelector("#answer").addEventListener("input", check, false);
 
         initializeTerms();
     });
