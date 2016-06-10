f1e111183d468ee2f5b397de70f136996e82c2a0
Oleg Samoylov <quillaur@outlook.com>
2016-06-11 02:33:00 +1000

    Fixes.

--- CC.Website/app/web/js/actions.js
+++ CC.Website/app/web/js/actions.js
@@ -26,7 +26,14 @@
     function check() {
         var q = document.getElementById("question");
         var a = document.getElementById("answer");
-        document.getElementById("addTerm").disabled = (q.value === "" || a.value === "") ? true : false;
+
+        if (typeof q === 'undefined' || q == null) return true;
+        var isQempty = q.value.replace(/\s/g, '').length < 1;
+
+        if (typeof a === 'undefined' || a == null) return true;
+        var isAempty = a.value.replace(/\s/g, '').length < 1;
+
+        document.getElementById("addTerm").disabled = (isQempty || isAempty) ? true : false;
     }
 
     WinJS.UI.processAll().then(function () {
@@ -34,6 +41,7 @@
         element.querySelector("#addTerm").addEventListener("click", addTerm, false);
         element.querySelector("#question").addEventListener("input", check, false);
         element.querySelector("#answer").addEventListener("input", check, false);
+        element.querySelector("#addTerm").disabled = true;
 
         initializeTerms();
     });
