dc5836343bb6b075c43ff7949c294fc9448bb50a
Oleg Samoylov <quillaur@outlook.com>
2016-06-12 00:00:41 +1000

    Transitions for launch.html.

--- CC.Website/app/web/js/actions.js
+++ CC.Website/app/web/js/actions.js
@@ -16,13 +16,15 @@
     function addTerm() {
         var term = {
             title: document.getElementById("question").value,
-            text: document.getElementById("answer").value
+            text: document.getElementById("answer").value.toLowerCase()
         };
 
         itemsList.push(term);
         document.getElementById("question").value = "";
         document.getElementById("answer").value = "";
+
         document.body.querySelector("#addTerm").disabled = true;
+        document.body.querySelector("#save").disabled = false;
     }
 
     function stringIsNullOrWhiteSpace(string)
@@ -38,12 +40,21 @@
         document.getElementById("addTerm").disabled = (stringIsNullOrWhiteSpace(q.value) || stringIsNullOrWhiteSpace(a.value)) ? true : false;
     }
 
+    function save() {
+        var NewScript = document.createElement('script')
+        NewScript.src = "js/fileSaver.js"
+        document.body.appendChild(NewScript);
+
+        saveTextAs("Hello, world!", "list.cwtf");
+    }
+
     WinJS.UI.processAll().then(function () {
         var element = document.body;
         element.querySelector("#addTerm").addEventListener("click", addTerm, false);
         element.querySelector("#question").addEventListener("input", check, false);
         element.querySelector("#answer").addEventListener("input", check, false);
-        element.querySelector("#addTerm").disabled = true;
+
+        element.querySelector("#save").addEventListener("click", save, false);
 
         initializeTerms();
     });
---
+++ CC.Website/app/web/js/launch.js
@@ -0,0 +1,3 @@
+﻿var header = document.getElementById("first-section");
+var content = document.getElementById("second-section");
+WinJS.UI.Animation.enterPage([[header], content], null);
