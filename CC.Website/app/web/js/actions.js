c538e1dc2fe32580881c25da366185465cd28b90
Oleg Samoylov <quillaur@outlook.com>
2016-06-17 17:51:06 +1000

    Сделано чтение XML-файлов в веб-приложении.

--- CC.Website/app/web/js/actions.js
+++ CC.Website/app/web/js/actions.js
@@ -30,6 +30,7 @@
         document.body.appendChild(NewScript);
     }
 
+    // Добавляет термин в коллекцию из формы.
     function addTerm() {
         var term = {
             title: document.getElementById("question").value,
@@ -44,29 +45,54 @@
         document.body.querySelector("#new").disabled = document.body.querySelector("#save").disabled = false;
     }
 
+    // Добавляет термин в коллекцию, используя готовые параметры.
+    function addTerm(answer, question) {
+        var term = {
+            title: question,
+            text: answer
+        };
+
+        itemsList.push(term);
+
+        document.body.querySelector("#addTerm").disabled = true;
+        document.body.querySelector("#new").disabled = document.body.querySelector("#save").disabled = false;
+    }
+
+    // Сохраняет файл, скоро будет DEPRECATED.
     function save() {
         saveTextAs("Hello, world!", "list.cwtf");
     }
 
+    // Открывает файл.
     function open() {
         document.body.querySelector("#input-b").click();
     }
 
-    function displayContents(contents) {
-        alert(contents);
-    }
-
+    // Читает файл, полученный из диалога открытия. 
     function change(e) {
         var file = e.target.files[0];
-        if (!file) {
+        if (!file)
             return;
-        }
         var reader = new FileReader();
         reader.onload = function(e) {
             var contents = e.target.result;
-            displayContents(contents);
+            parse(contents);
         };
         reader.readAsText(file);
+    }
+
+    // Преобразует XML-коллекцию в её JavaScript-эквивалент. 
+    function parse(xml) {
+        var xmlDoc = $.parseXML(xml);
+        var $xml = $(xmlDoc);
+
+        $xml.find('word').each(function () {
+            var id = $(this).find('ID').text();
+            var answer = $(this).find("answer").text();
+            var question = $(this).find("question").text();
+
+            addTerm(answer, question);
+        });
     }
 
     WinJS.UI.processAll().then(function () {
