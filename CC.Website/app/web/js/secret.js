73ab0d24fe64b8d6058cf10161339a37bd6fb444
Oleg Samoylov <quillaur@outlook.com>
2016-06-18 18:58:31 +1000

    test

---
+++ CC.Website/app/web/js/secret.js
@@ -0,0 +1,3 @@
+﻿var content = document.getElementById("page-text-header");
+var secondContent = document.getElementById("secret-div");
+WinJS.UI.Animation.enterPage([[content], secondContent], null);
