822ead2cc4a34a1aefa551f9f3494b0059ecae56
Oleg Samoylov <quillaur@outlook.com>
2016-06-22 20:23:00 +1000

    16:9- and mobile-friendly.

--- CC.Website/app/web/js/gridEditor.js
+++ CC.Website/app/web/js/gridEditor.js
@@ -27,10 +27,10 @@
         // Создание SVG.
         svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
         svg.setAttribute("style", "border: 1px solid #A9A9A9");
-        svg.setAttribute("width", 528);
+        svg.setAttribute("width", "100%");
         svg.setAttribute("height", 528);
         svg.setAttributeNS("http://www.w3.org/2000/xmlns/", "xmlns:xlink", "http://www.w3.org/1999/xlink");
-        element.querySelector("#secret-div").appendChild(svg);
+        element.querySelector("#svg-div").appendChild(svg);
 
         // For to be ready.
         svgNS = svg.namespaceURI;
