37ff8c73e0e1d78b534cbceb7c3bf57b2fb4fbae
Oleg Samoylov <quillaur@outlook.com>
2016-06-30 02:24:18 +1000

    *

--- CC.Website/js/actions.js
+++ CC.Website/js/actions.js
@@ -1,6 +1,7 @@
 ﻿if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
     var small = document.querySelector('#warning');
     small.style.display = 'block';
+    small.style.margin = '0px 0px 20px 0px';
 }
 
 var i = 0;
